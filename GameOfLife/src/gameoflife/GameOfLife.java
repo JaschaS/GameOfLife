30389a743178309871074c1010ba17ec18e80ef4
Jascha <J.scholz@fh-bingen.de>
2014-11-24 14:05:48 +0100

    Packages added

--- GameOfLife/src/gameoflife/GameOfLife.java
+++ GameOfLife/src/gameoflife/GameOfLife.java
@@ -22,7 +22,7 @@ public class GameOfLife extends Application {
     @Override
     public void start(Stage primaryStage) {
         Button btn = new Button();
-        btn.setText("Say 'Hello World'");
+        btn.setText("Dieses Spiel ist mist!");
         btn.setOnAction(new EventHandler<ActionEvent>() {
             
             @Override

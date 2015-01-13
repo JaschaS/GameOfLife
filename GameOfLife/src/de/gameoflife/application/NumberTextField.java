f11ded1c01f6dba63dcde13507eaf181784c684b
Jascha <J.scholz@fh-bingen.de>
2015-01-13 17:14:26 +0100

    NumberTextField informiert nun auch den listener, wenn das TextFeld den Focus verliert.

--- GameOfLife/src/de/gameoflife/application/NumberTextField.java
+++ GameOfLife/src/de/gameoflife/application/NumberTextField.java
@@ -5,6 +5,7 @@
  */
 package de.gameoflife.application;
 
+import javafx.beans.value.ObservableValue;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
@@ -15,118 +16,123 @@ import javafx.scene.input.KeyEvent;
  */
 public class NumberTextField extends TextField {
 
-    private NumberTextListener listener;  
+    private NumberTextListener listener;
     private final int min;
     private final int max;
 
-    public NumberTextField( double width, int min, int max, String startValue ) {
-    
-        super( startValue + "" );
-        
-        setPrefWidth( width );
-        
+    public NumberTextField(double width, int min, int max, String startValue) {
+
+        super(startValue + "");
+
+        setPrefWidth(width);
+
         this.min = min;
         this.max = max;
-        
-        setOnKeyPressed( (KeyEvent event) -> {
-            if( event.getCode() == KeyCode.ENTER ) {
-                
-                setFocused( false );
-                getParent().requestFocus();
-                
+
+        focusedProperty().addListener((ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) -> {
+            if (!newValue) {
+
                 int value = 0;
-                
-                if( getText().equals("") ) {
-                    
-                    setText( min + "" );
-                    
+
+                if (getText().equals("")) {
+
+                    setText(min + "");
+
                     value = min;
-                    
-                }
-                else {                
-                    
-                    value = Integer.parseInt( getText() );
-                    
-                    if( value > max ) {
-                        
-                        setText( max + "" );
+
+                } else {
+
+                    value = Integer.parseInt(getText());
+
+                    if (value > max) {
+
+                        setText(max + "");
                         value = max;
-                        
-                    }
-                    else {
-                        
-                        if( value < min ) {
-                            
-                            setText( min + "" );
+
+                    } else {
+
+                        if (value < min) {
+
+                            setText(min + "");
                             value = min;
-                            
+
                         }
-                        
+
                     }
-                    
+
                 }
-                
-                if( listener != null ) listener.inputHasChanged( value );
-                
+
+                if (listener != null) {
+                    listener.inputHasChanged(value);
+                }
+
             }
         });
-        
+
+        setOnKeyPressed((KeyEvent event) -> {
+            if (event.getCode() == KeyCode.ENTER) {
+
+                setFocused(false);
+                getParent().requestFocus();
+
+            }
+        });
+
     }
-    
+
     @Override
     public void replaceText(int start, int end, String text) {
-        
-        if( validate(text) ) {
-            
+
+        if (validate(text)) {
+
             //Text equals "" means the user used the backspace
-            if( text.equals("") ) {
-                
+            if (text.equals("")) {
+
                 text = getText(0, getText().length() - 1);
-               
+
                 super.setText(text);
-                
-            }
-            else {
-                
+
+            } else {
+
                 super.replaceText(start, end, text);
-                
+
             }
-            
+
         }
-        
+
     }
 
     @Override
     public void replaceSelection(String text) {
-        
-        if( validate(text) ) {
+
+        if (validate(text)) {
             super.replaceSelection(text);
         }
-        
+
     }
-    
-    public void setListener( NumberTextListener listener ) {
+
+    public void setListener(NumberTextListener listener) {
 
         this.listener = listener;
 
     }
-    
+
     public void removeListener() {
-    
+
         this.listener = null;
-        
+
     }
-    
+
     private boolean validate(String text) {
-        
+
         return text.equals("") || text.matches("[0-9]");
-        
+
     }
     
     public interface NumberTextListener {
-    
-        public void inputHasChanged( int value );
-        
+
+        public void inputHasChanged(int value);
+
     }
-    
+
 }

899355164f5d1f0e9ab4605116bb763dc7930e84
Jascha <J.scholz@fh-bingen.de>
2015-01-31 21:52:47 +0100

    dem TableColumn pattern und count ein CellValueFactory hinzufügen! Am Besten das Interface initilizable dem Controller implementieren lassen und dort das ganze hinzufügen.

--- src/de/gameoflife/application/ShowAnalysisController.java
+++ src/de/gameoflife/application/ShowAnalysisController.java
@@ -5,21 +5,11 @@
  */
 package de.gameoflife.application;
 
-import java.net.URL;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.ResourceBundle;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.GridPane;
 
 /**
  *
